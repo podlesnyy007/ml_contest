e10a6cdb98c127ab45e6434a6f645d8a0c2f9c21
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2025-04-03 03:22:29 +0400

    Create 05.py

---
+++ 3.1 Множества/05.py
@@ -0,0 +1,4 @@
+mset1=set(input())
+mset2=set(input()) #поменьше
+print(mset1.intersection(mset2)==mset2)
+#intersection() находит пересечение двух множеств, то есть общие элементы между mset1 и mset2
