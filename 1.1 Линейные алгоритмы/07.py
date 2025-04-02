9c5ed4554a658b72df5d058219ade66863506e0e
mATVEY <114169264+podlesnyy007@users.noreply.github.com>
2025-04-02 22:10:08 +0400

    Create 07.py

---
+++ 1.1 Линейные алгоритмы/07.py
@@ -0,0 +1,21 @@
+def max_squares(a, b, k):
+    # Количество квадратов, которые помещаются вдоль стороны a и b
+    squares_a = a // k
+    squares_b = b // k
+    # Общее количество квадратов
+    return squares_a * squares_b
+
+# Пример использования:
+a = int(input())
+b = int(input())
+k = int(input())
+
+print(max_squares(a, b, k))
+
+"""
+2
+3
+1
+-----
+6
+"""
